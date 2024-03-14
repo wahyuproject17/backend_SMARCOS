addecc4489c3e908b81c58e28a90efc9aaa96088
wahyuproject17 <wahyuaguz9@gmail.com>
2024-03-14 23:24:54 +0700

    Create admin.js

---
+++ src/controllers/admin.js
@@ -0,0 +1,43 @@
+const database = require('../initializers/database');
+
+let mysql = require('mysql');
+let pool = mysql.createPool(database);
+
+pool.on('error',(err)=> {
+    console.error(err);
+});
+
+module.exports ={
+    saveRegister(req,res){
+        let username = req.body.username;
+        let nama_lengkap = req.body.namalengkap;
+        let no_hp = req.body.nohp;
+        let email = req.body.email;
+        let alamat = req.body.alamat;
+        let password = req.body.pass;
+
+        if (username && nama_lengkap && no_hp && email && alamat && password) {
+    
+            pool.getConnection(function(err, connection) {
+                if (err) throw err;
+                connection.query(
+                    `INSERT INTO tbl_user (username, nama_lengkap, no_hp, email, alamat, password) VALUES (?,?,?,?,SHA2(?,512));`
+                , [username, nama_lengkap, no_hp, email, alamat, password],function (error, results) {
+                    if (error) throw error;
+              
+                    req.flash('color', 'success');
+                    req.flash('status', 'Yes..');
+                    req.flash('message', 'Registrasi berhasil');
+                    
+                    res.redirect('/login');
+                });
+              
+                connection.release();
+            })
+        } else {
+            
+            res.redirect('/login');
+            res.end();
+        }
+    }
+}
